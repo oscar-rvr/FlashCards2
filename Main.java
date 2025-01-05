ee94fc9df5a1c9ebabb73e8ccacfb21ff87c67be
oscar vega <oscar_rvr@outlook.com>
2025-01-05 13:35:42 -0600

    fase1 done

--- Main.java
+++ Main.java
@@ -1,26 +1,21 @@
 package flashcards;
 
 import java.util.Scanner;
-//fase 2
+
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String term="";
-        String definition="";
-        String answer="";
+        String cardInput ="";
+        String definitionInput ="";
 
-        term = scan.nextLine();
-        definition = scan.nextLine();
-        answer = scan.nextLine();
-        scan.close();
-        if(definition.equals(answer)){
-            System.out.println("Your answer is right!");
-        }else{
-            System.out.println("Your answer is wrong...");
-        }
+        System.out.println("Card:");
+        cardInput = scan.nextLine();
+        System.out.println(cardInput);
 
 
-
+        System.out.println("Definition:");
+        definitionInput = scan.nextLine();
+        System.out.println(definitionInput);
 
     }
 }
