7830a46e7f19713b9921bfeb281a3c21edc92b47
oscar vega <oscar_rvr@outlook.com>
2025-01-05 20:44:51 -0600

    Finished fase4

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 package flashcards;
-//fase 3 solo e .main
+//fase 04
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -20,11 +20,26 @@ public class Main {
         for (int i =0; i<cardN; i++){
 
             System.out.println("Card #"+(i+1)+":");
-
             term=scan.nextLine();
-
+          /* if(existTerm(term,map)){
+               System.out.println("The term \""+ term +"\" already exists. Try again:");
+               continue;
+           }*/
+           while(existTerm(term,map)){
+               System.out.println("The term \""+ term +"\" already exists. Try again:");
+               //System.out.println("Card #"+(i+1)+":");
+               term=scan.nextLine();
+           }
             System.out.println("The definition for card #"+(i+1)+":");
             definition=scan.nextLine();
+
+            while(existDef(definition,map)){
+                System.out.println("The definition \""+definition+"\" already exists. Try again:");
+                //System.out.println("Card #"+(i+1)+":");
+                definition=scan.nextLine();
+            }
+
+
 
             //fullfilling
             map.put(term,definition);
@@ -40,10 +55,49 @@ public class Main {
            if(aux.equals(defMap))
            {
                System.out.println("Correct!");
+           }else if(map.containsValue(defMap)){
+               System.out.println("Wrong. The right answer is \""+   defMap  +"\", but your definition is correct for \""+getKey(aux,map)+"\"");
            }else{
                System.out.println("Wrong. The right answer is \""+defMap+"\".");
+
            }
        }
 
+    }
+
+    public static boolean existTerm(String term, LinkedHashMap<String,String> map){
+        for (Map.Entry<String,String> entry : map.entrySet()){
+            String termMap = entry.getKey();
+            String defMap = entry.getValue();
+            if(term.equals(termMap) ){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean existDef(String def, LinkedHashMap<String,String> map){
+        for (Map.Entry<String,String> entry : map.entrySet()){
+            String termMap = entry.getKey();
+            String defMap = entry.getValue();
+
+            if(def.equals(defMap) ){
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    public static String getKey(String value, LinkedHashMap<String,String> map){
+        for (Map.Entry<String, String> entry : map.entrySet()){
+            if(entry.getValue().equals(value)){
+                return entry.getKey();
+            }
+
+
+        }
+        return null;
     }
 }
