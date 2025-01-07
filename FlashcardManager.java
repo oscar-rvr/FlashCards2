bef8c4d12d4de0886b7a0d22889e17e5efd75a48
Oscar Vega Mac <oscar_rvr@outlook.com>
2025-01-07 17:24:55 -0600

    stage5 completed refactorized to Objects

--- FlashcardManager.java
+++ FlashcardManager.java
@@ -1,9 +1,7 @@
 package flashcards;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Random;
+import java.io.*;
+import java.util.*;
 
 public class FlashcardManager {
     private Map<String,Flashcard> flashcards;
@@ -15,8 +13,24 @@ public class FlashcardManager {
     public void addCard(String term, String definition){
         Flashcard card = new Flashcard(term,definition);
         flashcards.put(term,card);
-        System.out.println("The pair (\"" + term + "\":\"" + definition + "\") has been added.");
+
     }
+    public boolean existsTerm(String term){
+        if(flashcards.containsKey(term)){
+            return true;
+        }
+        return false;
+    }
+public boolean existDefinition(String definition){
+        for(Map.Entry<String, Flashcard> entry : flashcards.entrySet()){
+            if (entry.getValue().getDefinition().equals(definition)){
+                return true;
+            }
+        }
+        return false;
+
+}
+
 
     public void removeCard(String term){
         if (flashcards.containsKey(term)){
@@ -28,21 +42,77 @@ public class FlashcardManager {
     }
 
     public void askCard(int times) {
-        Random rand = new Random();
-        String[] terms = flashcards.keySet().toArray(new String[0]);
-        String randomTerm = terms[rand.nextInt(terms.length)];
-        Flashcard card = flashcards.get(randomTerm);
+        for (int i =0; i< times; i++){
+            Random rand = new Random();
+            String[] terms = flashcards.keySet().toArray(new String[0]);
+            String randomTerm = terms[rand.nextInt(terms.length)];
+            Flashcard card = flashcards.get(randomTerm);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Print the definition of \"" + randomTerm + "\":");
-        String userAnswer = scanner.nextLine();
-        if (userAnswer.equals(card.getDefinition())) {
-            System.out.println("Correct!");
-        } else {
-            System.out.println("Wrong. The right answer is \"" + card.getDefinition() + "\".");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Print the definition of \"" + randomTerm + "\":");
+            String userAnswer = scanner.nextLine();
+            if (userAnswer.equals(card.getDefinition())) {
+                System.out.println("Correct!");
+            } else if(!whereDefinition(userAnswer).isEmpty()) {
+                System.out.println("Wrong. The right answer is \""+card.getDefinition()+"\", but your definition is correct for \""+whereDefinition(userAnswer)+"\".");
+            }else{
+                System.out.println("Wrong. The right answer is \"" + card.getDefinition() + "\".");
+               // System.out.println(!whereDefinition(userAnswer).isEmpty());
+            }
         }
+
     }
 
+    public String whereDefinition(String definition){
+        for(Map.Entry<String, Flashcard> entry : flashcards.entrySet()){
+            if (entry.getValue().getDefinition().equals(definition)){
+                return entry.getValue().getTerm().toString();
+            }
+        }
+        return "";
+
+    }
+    public void export(String filename){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename))){
+            for (  Map.Entry<String,Flashcard> entry : flashcards.entrySet() ){
+                      String term = entry.getKey();
+                      String definition = (entry.getValue().getDefinition());
+                      writer.write(term+","+definition);
+                      writer.write("\n");
+            }
+           // writer.close();
+            writer.flush();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
+        System.out.println(getCardCount()+" cards have been saved.");
+    }
+
+    public void importF(String filename){
+ //flashcards.clear();
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename))){
+            String line;
+
+
+           int contI=0;
+            while( (line = reader.readLine()) != null ){
+                String term,definition;
+                String[] cardReaded = line.split(",");
+                term=cardReaded[0];
+                definition=cardReaded[1];
+
+
+                contI++;
+                addCard(term,definition);
+            }
+            System.out.println(contI+" cards have been loaded.");
+        } catch (IOException e) {
+            //throw new RuntimeException(e);
+            System.out.println("File not found.");
+        }
+    }
     public int getCardCount(){
         return flashcards.size();
     }
--- Main.java
+++ Main.java
@@ -15,15 +15,29 @@ public class Main {
         FlashcardManager manager = new FlashcardManager();
         String action="";
         do {
-            System.out.println("\nInput the action (add, remove, ask, exit):");
+            System.out.println("\nInput the action (add, remove, import, export, ask, exit):");
             action = scan.nextLine();
             if (action.equals("add")) {
                 // Agregar una tarjeta
                 System.out.println("The card:");
                 String term = scan.nextLine();
-                System.out.println("The definition of the card:");
-                String definition = scan.nextLine();
-                manager.addCard(term, definition);
+                if(manager.existsTerm (term)){
+                    System.out.println("The card \""+term+"\" already exists.");
+
+                }else{
+                    System.out.println("The definition of the card:");
+                    String definition = scan.nextLine();
+
+                    if(manager.existDefinition(definition)){
+                        System.out.println("The definition \""+definition+"\" already exists.");
+                    }else{
+                        manager.addCard(term, definition);
+                        System.out.println("The pair (\"" + term + "\":\"" + definition + "\") has been added.");
+
+                    }
+                }
+
+
 
             } else if (action.equals("remove")) {
                 // Eliminar una tarjeta
@@ -35,8 +49,19 @@ public class Main {
                 // Preguntar por una tarjeta
                 System.out.println("How many times to ask?");
                 int times = scan.nextInt();
+                manager.askCard(times);
+
+            } else if(action.equals("export")){
+                System.out.println("File name:");
+                String filename = scan.nextLine();
+                manager.export(filename);
 
 
+            }else if(action.equals("import")){
+
+                System.out.println("File name:");
+                String importFile = scan.nextLine();
+                manager.importF(importFile);
             }
 
 
